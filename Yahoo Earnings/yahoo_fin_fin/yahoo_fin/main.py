9255c332117ddebdd7dacb77897a357a6e81c8d2
giannisp01 <gpanis01@ucy.ac.cy>
2022-08-30 11:44:41 +0300

    m

--- Yahoo Earnings/yahoo_fin_fin/yahoo_fin/main.py
+++ Yahoo Earnings/yahoo_fin_fin/yahoo_fin/main.py
@@ -10,9 +10,11 @@ counter = 0
 with open('company_tickers.csv', mode='r') as file:
     csvFile = csv.reader(file)
     for lines in csvFile:
-        print(f"Company {counter} : {lines[2]} ", end=" ")
         data = si.get_earnings_history(lines[2])
-        print(f"{lines[2]} length data : {len(data)}")
+        if counter%25==0 and counter!=0:
+            import time
+            print("Sleeping")
+            time.sleep(120)
         counter += 1
 
 
--- Yahoo Earnings/yahoo_fin_fin/yahoo_fin/stock_info.py
+++ Yahoo Earnings/yahoo_fin_fin/yahoo_fin/stock_info.py
@@ -743,19 +743,14 @@ def _parse_earnings_json(url, headers={
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
                          ):
 
-    s = requests.Session()
-    resp = s.get(url, headers=headers)
+    resp = requests.get(url, headers=headers)
     content = resp.content.decode(encoding='utf-8', errors='strict')
-
-    #resp = requests.get(url, headers=headers, cookies=False)
-    #content = resp.content.decode(encoding='utf-8', errors='strict')
 
     page_data = [row for row in content.split(
         '\n') if row.startswith('root.App.main = ')][0][:-1]
 
     page_data = page_data.split('root.App.main = ', 1)[1]
 
-    s.close()
     return json.loads(page_data)
 
 
@@ -794,9 +789,10 @@ def get_earnings_history(ticker):
                 _parse_earnings_json(url)["context"]["dispatcher"]["stores"]["ScreenerResultsStore"]["results"]["rows"]
                 ready = True
             except IndexError:
-                print("Banned...Retrying in 10 seconds")
+                print("Banned...Retrying in 180 seconds", end=' ')
+                print(ticker)
                 import time
-                time.sleep(10)
+                time.sleep(180)
         ticker_earnings = ticker_earnings + result
         first = False
 
