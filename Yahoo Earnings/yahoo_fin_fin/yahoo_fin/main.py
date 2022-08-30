ce6dd447892d59efe9dcfc1a2d4624aa38eba251
giannisp01 <gpanis01@ucy.ac.cy>
2022-08-30 11:23:10 +0300

    m

--- Yahoo Earnings/yahoo_fin_fin/yahoo_fin/main.py
+++ Yahoo Earnings/yahoo_fin_fin/yahoo_fin/main.py
@@ -1,11 +1,18 @@
 import stock_info as si
 import csv
-counter=0
-with open('company_tickers.csv', mode ='r')as file:
+from requests import get
+
+ip = get('https://api.ipify.org').text
+
+data = si.get_earnings_history('MSFT')
+print(f'My public IP address is: {ip}')
+counter = 0
+with open('company_tickers.csv', mode='r') as file:
     csvFile = csv.reader(file)
     for lines in csvFile:
-
         print(f"Company {counter} : {lines[2]} ", end=" ")
         data = si.get_earnings_history(lines[2])
         print(f"{lines[2]} length data : {len(data)}")
-        counter+=1
+        counter += 1
+
+
